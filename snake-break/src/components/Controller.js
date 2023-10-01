61f8f40b90df29b3dce0d4f5c7deb899201b248e
Jordan Olguin <jdolguin62@gmail.com>
2023-09-30 18:34:40 -0600

    created Controller component, prestyles

---
+++ snake-break/src/components/Controller.js
@@ -0,0 +1,33 @@
+const Controller = ({ onUp, onDown, onLeft, onRight }) => {
+  return (
+    <div className="controller">
+      <div className="up">
+        <input className="upwards" onClick={onUp} type="button" value="Up" />
+      </div>
+      <div className="left-right">
+        <input
+          className="leftwards"
+          onClick={onLeft}
+          type="button"
+          value="Left"
+        />
+        <input
+          className="rightwards"
+          onClick={onRight}
+          type="button"
+          value="Right"
+        />
+      </div>
+      <div className="down">
+        <input
+          className="downwards"
+          onClick={onDown}
+          type="button"
+          value="Down"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Controller;
